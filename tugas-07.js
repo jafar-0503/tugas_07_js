73d6e8299064ee7777ec6c65c7298f35efb10270
Mukhamat Jafar <jafar.mukhamat7@gmail.com>
2019-07-30 23:19:54 +0700

    tugas_07_js
    tugas sudah ok !!!

---
+++ tugas-07.js
@@ -0,0 +1,15 @@
+
+
+var tinggi = [150, 155, 160, 165, 170, 175, 180, 185, 190, 195];
+
+// Perulangan Biasa
+for (var i = 0; i < tinggi.length; i++) {
+  console.log(tinggi[i]);
+
+}
+console.log("");
+
+// Perulangan for-of
+for (let x of tinggi) {
+  console.log(x);
+}
